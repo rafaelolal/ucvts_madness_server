c102483a69bfb28109ee35ad717442a22247fef8
Rafael Almeida <rafaelpbcp@gmail.com>
2023-04-25 23:36:28 -0400

    preparing for hosting

--- ucvts_madness_server/wsgi.py
+++ ucvts_madness_server/wsgi.py
@@ -4,13 +4,25 @@ WSGI config for ucvts_madness_server project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
-from django.core.wsgi import get_wsgi_application
+# add the virtualenv site-packages path to the sys.path
+site_packages = '/home/rafaelolal/envs/ucvts_madness_server_env/lib/python3.10/site-packages'
+if site_packages not in sys.path:
+    sys.path.append(site_packages)
+
+project_path = '/home/rafaelolal/ucvts_madness_server'
+project_path2 = '/home/rafaelolal/ucvts_madness_server/ucvts_madness_server'
+
+if project_path not in sys.path:
+    sys.path.append(project_path)
+if project_path2 not in sys.path:
+    sys.path.append(project_path2)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ucvts_madness_server.settings')
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
